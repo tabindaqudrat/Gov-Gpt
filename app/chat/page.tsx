a6b6d782d5763c27eea5db52809e462519a77d89
Masna Bin Umeed <masnabinumeed@gmail.com>
2024-12-03 18:51:36 +0500

    fixes to remove scroll

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -148,7 +148,7 @@ export default function ChatPage() {
   }, [messages])
 
   return (
-    <div className="flex flex-col lg:flex-row w-full h-screen overflow-hidden touch-manipulation">
+    <div className="flex flex-col lg:flex-row w-full  overflow-hidden touch-manipulation">
       <MessageThreadsSidebar
         isOpen={isSidebarOpen}
         onClose={() => setIsSidebarOpen(false)}
@@ -184,8 +184,7 @@ export default function ChatPage() {
         </div>
 
         {/* Messages */}
-        <div className="flex-1 overflow-y-auto">
-          <div className="flex flex-col max-w-2xl mx-auto p-4">
+          <div className="flex flex-col max-w-2xl mx-auto p-4 mt-auto">
             {messages.map((message) => (
               <ChatBubble
                 key={message.id}
@@ -215,7 +214,6 @@ export default function ChatPage() {
             ))}
             <div ref={messagesEndRef} />
           </div>
-        </div>
 
         {/* Input */}
         <div className="flex-none p-4">
